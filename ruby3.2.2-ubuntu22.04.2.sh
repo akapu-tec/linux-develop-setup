5e94fdb93737febd7c226a8b1df7199f014e4557
Gedean Dias <gedean.dias@gmail.com>
2023-08-26 18:11:42 -0300

    save point

--- ruby3.2.2-ubuntu22.04.2.sh
+++ ruby3.2.2-ubuntu22.04.2.sh
@@ -18,8 +18,7 @@
 # e.g.
 # wsl --set-version Ubuntu-20.04 2
 
-## Do prefer reset wsl installation:
-# wsl --unregister Ubuntu-22.04
+## Do prefer reset wsl installation: wsl --unregister Ubuntu-22.04
 
 ### Ubuntu
 sudo apt update
